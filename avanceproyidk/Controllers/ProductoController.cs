47722db09c4a307ae7a71077175cc31547e11d6b
Renato Abanto <128441564+DarcyRekk@users.noreply.github.com>
2023-11-30 18:26:09 -0500

    Update ProductoController.cs

--- avanceproyidk/Controllers/ProductoController.cs
+++ avanceproyidk/Controllers/ProductoController.cs
@@ -43,12 +43,12 @@ namespace avanceproyidk.Controllers
             ProductosEntity entity = new ProductosEntity();
             entity.nombreproducto = model.nombreproducto;
             entity.marca = model.marca;
-            entity.precio = model.precio.HasValue ? model.DNI.Value : 0;
+            entity.precio = model.precio;
             entity.descuento =model.descuento;
             entity.preciofinal = model.preciofinal;
-            entity.stock = model.stock.HasValue ? model.Celular.Value : 0;
+            entity.stock = model.stock;
             entity.descripcion = model.descripcion;
-            entity.imagen = model.imagen.HasValue ? model.TelefonoContacto.Value : 0;
+            entity.imagen = model.imagen;
             _productosContext.Productos.Add(entity);
             _productosContext.SaveChanges();
             return RedirectToAction("List", "Productos");
